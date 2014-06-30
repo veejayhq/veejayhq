75830cf00ce273a60c11ff1505678352b81f4323
Matthijs van Henten <matthijs@ischen.nl>
2014-06-30 12:30:17 +0200

    cleanups in templates, adding simple bash script to bake the site

---
+++ build-site.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TARGET_DIR=veejayhq.github.io
+#rm -rf ${TARGET_DIR}
+#git clone git@github.com:veejayhq/veejayhq.github.io.git ${TARGET_DIR}
+./cmd.js build content ${TARGET_DIR}
+cd ${TARGET_DIR}
+git add -u
+git commit
+git push
