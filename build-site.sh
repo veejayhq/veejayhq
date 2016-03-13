2305260d2985dafa765fbcfdf373e781094d86f7
Matthijs van Henten <matthijs+foo@c9.io>
2016-03-13 22:52:40 +0000

    remove cname in build script

--- build-site.sh
+++ build-site.sh
@@ -5,7 +5,7 @@ rm -rf ${TARGET_DIR}
 git clone git@github.com:veejayhq/veejayhq.github.io.git ${TARGET_DIR}
 ./cmd.js build content ${TARGET_DIR}
 cd ${TARGET_DIR}
-echo "veejayhq.net" > CNAME
+# echo "veejayhq.net" > CNAME
 git add -u
 git commit -m "Regenerated website on $(date)"
 git push
